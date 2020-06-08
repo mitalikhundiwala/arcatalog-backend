d4242d64a58c26a4b4b3d54af5cfa79affafdd59
Mitali Patel <mitali.khundiwala@gmail.com>
2020-06-08 19:00:04 +0400

    error fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { initializeApp } from './app';
 
 const app = initializeApp();
 
-app.listen([{ port: process.env.PORT || 4000 }]).then((deets) => {
+app.listen({ port: process.env.PORT || 4000 }).then((deets) => {
     console.log(`Server is now running on port
   http:/localhost:${deets.port}`);
     console.log('url', deets.url);
