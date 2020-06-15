4dee091746fa91b5d113dc4705cfc868cce42409
Mitali Patel <mitali.khundiwala@gmail.com>
2020-06-15 18:04:12 +0400

    added firebase production settings

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { initializeApp } from './app';
 
 const app = initializeApp();
 
+console.log(process.env.NODE_ENV);
+
 app.listen({ port: process.env.PORT || 4000 }).then((deets) => {
     console.log(`Server is now running on port
   http:/localhost:${deets.port}`);
-    console.log('url', deets.url);
 });
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,10 @@
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require('../../ar-catalog-firebase.json');
+const serviceAccount =
+    process.env.NODE_ENV === 'production'
+        ? require('../../ar-catalog-production-firebase.json')
+        : require('../../ar-catalog-firebase.json');
+console.log(serviceAccount);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
